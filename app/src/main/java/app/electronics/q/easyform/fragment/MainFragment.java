e195169af974ee82e31fa68a1db559cf36d59f37
Atthana <atthana.p@scale360solutions.com>
2017-09-17 10:51:50 +0700

    Add Fragment OK

---
+++ app/src/main/java/app/electronics/q/easyform/fragment/MainFragment.java
@@ -0,0 +1,26 @@
+package app.electronics.q.easyform.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import app.electronics.q.easyform.R;
+
+/**
+ * Created by Atthana on 9/17/2017.
+ */
+
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+
+} //Main class
+
